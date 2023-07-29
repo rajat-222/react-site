0c1aa62a33bbf78eda573685df6e8d7d135b452f
rajat-222 <rajatbisht222@gmail.com>
2023-07-29 13:19:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Navabar from './Components/Navbar/Navabar';
+// import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+          <Navabar/>
+          <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          </Routes>
+          
+    </Router>
+      
+    </>
   );
 }
 
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,274 @@
+import React from 'react';
+import './Home.css';
+import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <>
+        <section className="container section-1">
+        <div className="slogan">
+            <div className="headingcontainer">
+              <br/>
+              <br/>
+              <br/>
+              
+              <br/>
+              <br/>
+              <br/>
+              
+                {/* /*</div><h1>Microsoft <span className="txt-type" data-wait="3000" data-words='["Technical Community"]'></span></h1> */}
+                <span>
+                  Microsoft Technical Community
+                </span>
+                <br/>
+                <h2><b>Technology is making people's lives easier.
+                    We, here at MTC are, constantly
+                    Innovating, Inventing and Improvising.</b></h2>
+
+                    <br/>
+                    <br/>
+              
+                    <div className="containerbtn">
+                      <Link to="/about"><button>About US</button></Link>
+                      <button><Link to="/about">event</Link></button>
+                    </div> 
+
+              </div>
+        </div>
+
+
+        <div className="home-computer-container">
+       {/* <!--   <img className="home-computer" src="https://github.com/r-e-d-ant/Computer-store-website/blob/main/assets/images/home_img.png?raw=true" alt="a computer in dark with shadow" className="home-img">--> */}
+       <div className="logo-container">
+        {/* <h1 id="page-logo">     <img src="images/Mtc.png"/></h1> */}
+      </div>
+
+        </div>
+
+       <section className="logobox">
+
+        <section className="wrapper">
+        <div className="one"></div>
+        <div className="one1"></div>
+         <div className="one2"></div>
+         <div className="one3"></div>
+        <div className="one4"></div>
+         <div className="one5"></div>
+         <div className="one6"></div>
+        <div className="one7"></div>
+         <div className="one8"></div>
+        <div className="one9"></div>
+         <div className="one10"></div>
+         <div className="one11"></div>
+         <div className="one12"></div>
+         <div className="one13"></div>
+         <div className="one14"></div>
+         <div className="one15"></div>
+         <div className="one16"></div>
+         <div className="one17"></div>
+        <div className="one18"></div>
+        <div className="one19"></div>
+        <div className="one20"></div>
+ 
+        </section>
+        </section>
+        
+
+
+         
+    <div className="area" >
+      <ul className="circles">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+      </ul>
+</div > 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </section>
+
+     
+      <section id="about" className="about">
+
+
+<div className="abtimg">
+<img src="images/back.JPG" alt="" style={{width:600, height:500}}/>
+</div>
+
+<div className="abttxt">
+  <h1><strong>ABOUT US</strong></h1>
+</div>
+
+
+
+<div className="abtmtc">
+  Microsoft Technical Community was established in February 2018 <br/>with the vision of “Innovating, Inventing and Improvising” to educate fellow <br/>computer science 
+  enthusiasts about various aspects of the technical world.<br/> The goal was and is to create professionals out of amateurs and <br/>to teach not just 
+  Microsoft technologies but anything that is required <br/> for students to excel in their careers.
+   We have come a long way since then, <br/>and still have many more goals to reach..</div>
+
+
+<div className="mtcc">
+  Microsoft Technical Community only focuses on technical events<br/> and approaches them with complete professionalism.<br/>
+   ‘Pathshala’ sessions, ‘Career Pathway’ sessions, ‘Buggymania’<br/> and ‘Career Drive’our main flagship event are just some of the events<br/> we conduct over a year
+    with the sole purpose of creating better<br/> IT professionals. The team at Microsoft Technical Community is full of <br/>hard-working and professional individuals,
+     who are always<br/> learning and teaching something new.
+</div>
+</section>
+  
+
+  
+
+
+      <section id="pastsection">
+
+           
+        <center>
+          <br/>
+          <h1 style={{fontSize: 100,
+          color: rgb(0, 0, 0),}}><b>Our Past Event</b></h1>  
+            </center>
+  
+        <div className="gallery-container">
+          <div className="gallery">
+           
+
+            <div className="frame"><img className="image" src="images/1.jpeg" />
+              <br/>
+              <div className="info">Java Pathsala</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/2.png"/>
+              <div className="info">Codivent</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/3.png"/>
+              <br/>
+              <div className="info">&nbsp;&nbsp;&nbsp;Qurantime &nbsp;&nbsp;&nbsp;Programmer</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/4.png"/>
+              <br/>
+              <div className="info">Web-Portfolio</div>
+            </div>
+            <div className="frame"><img className="image" src="images/5.png"/>
+              <br/>
+              <div className="info">Buggymania 2.0</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/6.png"/>
+              <br/>
+              <div className="info">Infosec Talk</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/7.png"/>
+              <br/>
+              <div className="info">C++ Paathshala</div>
+            </div>
+
+            <div className="frame"><img className="image" src="images/8.jpg"/>
+              
+              <div className="info">Cloud Computing</div>
+            </div>
+
+         
+          </div>
+        </div>
+
+      
+        <div className="pastbg">
+          <img src="images/past_full_bg.png" alt=""/>
+        </div>
+      <section className="sponsorsection">
+
+          <center>
+            <br/>
+          
+            <br/>
+            <br/>
+        <h1 style="font-size: 100px;color: rgb(15, 15, 134);"><b>Our Sponsor</b></h1> 
+        </center> 
+
+        <br/>
+        <br/>
+        
+<div className="sponsorbox1">
+<img src="images/10.png" style={{height:210}}/>
+<div className="sponsortxt">
+  <h4 style={{color: rgb(15, 15, 134), fontSize: 30, fontWeight: 900,}}><br/>Coding Ninja </h4>Coding Ninjas is a place that trains passionate people in various technologies. Our core programs are intensive, immersive training that transforms people into outstanding developers. The training is provided by expert faculties who have graduated from esteemed Universities such as Stanford, IITs and IITs. They have valuable teaching experience and extensive knowledge which they share with students to guide them in becoming a great programmer or a developer."</div>
+</div>
+
+
+<br/>
+<br/>
+
+
+<div className="sponsorbox1">
+  <img src="images/40.png" style={{height:210}}/>
+  <div className="sponsortxt">
+    <br/>
+    <h4 style="color: rgb(15, 15, 134); font-size: 30px; font-weight: 900;">Zuno </h4><br/>Zuno is a platform that offers paid internships and jobs for freshers. With thousands of openings on our platform, we are on our way to helping students and fresh graduates find the right career opportunities to kickstart their careers.</div>
+  </div>
+
+
+<br/>
+<br/>
+
+
+  <div className="sponsorbox1">
+    <img src="images/20.png" style={{height:210}}/>
+    <div className="sponsortxt">
+      <br/>
+      <h4 style="color: rgb(15, 15, 134); font-size: 30px; font-weight: 900;">Microsoft </h4><br/>Microsoft Corporation is an American multinational technology company that develops, licenses, and sells computer software, consumer electronics, and personal computers. It is best known for its Windows operating system, Microsoft Office Suite, and Xbox gaming console.</div>
+    </div>
+
+
+<br/>
+<br/>
+
+
+
+
+    <div className="sponsorbox1">
+      <img src="images/30..png" style={{height:210}}/>
+      <div className="sponsortxt">
+        <h4 style="color: rgb(15, 15, 134); font-size: 30px; font-weight: 900;"><br/>Wayspire </h4><br/>
+        
+"WAYSPIRE ED-TECH PVT LTD is a dedicated E-learning platform to creating a community of lifelong learners. An E-learning platform with the goal of competence to students for the workforce and assisting you in landing your desired Job/ University.</div>
+      </div>
+
+      </centre>
+      </section>
+
+
+
+    </>
+  );
+}
+
+export default Home;
