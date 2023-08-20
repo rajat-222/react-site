5e4f692bef3470602cd898de7090db6ce508d0b7
Priyanshu <priyanshubutola@gmail.com>
2023-08-20 13:01:05 +0530

    new home page(canva design) and footer(canva design)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Home/Home';
-import Navabar from './Components/Navbar/Navabar';
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+// import Home from './Components/Home/Home';
+import HomePage from "./Components/Home/HomePage";
+import Navabar from "./Components/Navbar/Navabar";
 // import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-          <Navabar/>
-          <Routes>
-          <Route path='/' exact element={<Home/>}/>
-          </Routes>
-          <Footer/>
-          
-    </Router>
-      
+      <Router>
+        <Navabar />
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
